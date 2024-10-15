f33964aedfdf088d45444da30fcb7302e44c6820
Dimas Agung <samidagunh12@gmail.com>
2024-10-15 09:31:24 +0700

    menambah button

--- resources/views/user/editUser.blade.php
+++ resources/views/user/editUser.blade.php
@@ -10,56 +10,62 @@
                             <a href="{{asset('/admin/user/list')}}" class="btn btn-secondary"><i class="fa-solid fa-arrow-left"></i></a>
                         </div>
                         <div class="col-7">
-                            <h3>Edit User</h3>
+                            <h3>Tambah User</h3>
                         </div>
                     </div>
                 </div>
                 <div class="card-body">
-                    <div class="row">
-                        <div class="col-8 mt-4">
-                            <label>Nama:</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Judul Materi">
-                        </div>
-                        <div class="col-8 mt-4">
-                            <label>User Name:</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Judul Materi">
-                        </div>
-                        <div class="col-8 mt-4">
-                            <label>Password:</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama Mentee">
-                        </div>
-                        <div class="col-8 mt-4">
-                            <label>Asal Instanasi:</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama Mentee">
-                        </div>
-                        <div class="col-4">
-                           
-                        </div>
-                        <div class="col-4 mt-4">
-                            <div class="form-group">
-                                <label>Periode magang</label>
-                                <select class="form-control select2" style="width: 100%;">
-                                    <option selected="selected">Alabama</option>
-                                    <option>Alaska</option>
-                                    <option>California</option>
-                                    <option>Delaware</option>
-                                    <option>Tennessee</option>
-                                    <option>Texas</option>
-                                    <option>Washington</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div class="col-4 mt-4">
-                            <label>Status:</label>
-                            <select class="form-control select2" style="width: 100%;">
-                                <option selected="selected">Enable</option>
-                                <option>Disable</option>
-                        </div>
-                </div>
+    <div class="row">
+        <div class="col-8 mt-4">
+            <label>Nama:</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Judul Materi">
+        </div>
+        <div class="col-8 mt-4">
+            <label>User Name:</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Judul Materi">
+        </div>
+        <div class="col-8 mt-4">
+            <label>Password:</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama Mentee">
+        </div>
+        <div class="col-8 mt-4">
+            <label>Asal Instanasi:</label>
+            <input type="text" class="form-control" id="exampleInputEmail1" placeholder="Nama Mentee">
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4 mt-4">
+            <div class="form-group">
+                <label>Periode magang</label>
+                <select class="form-control select2" style="width: 100%;">
+                    <option selected="selected">Alabama</option>
+                    <option>Alaska</option>
+                    <option>California</option>
+                    <option>Delaware</option>
+                    <option>Tennessee</option>
+                    <option>Texas</option>
+                    <option>Washington</option>
+                </select>
             </div>
+        </div>
+        <div class="col-4 mt-4">
+            <label>Status:</label>
+            <select class="form-control select2" style="width: 100%;">
+                <option selected="selected">Enable</option>
+                <option>Disable</option>
+            </select>
+        </div>
+    </div>
+    <div class="container mt-3">
+        <div class="d-flex justify-content-end">
+            <button type="button" class="btn btn-success">Submit</button>
         </div>
     </div>
 </div>
+
+        </div>
+    </div>
+</div>
+
 
 <script src="{{asset ('plugins/jquery/jquery.min.js')}}"></script>
 
